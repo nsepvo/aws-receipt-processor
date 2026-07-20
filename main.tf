76ac3a8291ae778d1b46bfcf04059c978ac94b8d
neven <nevens2003@gmail.com>
2026-07-20 06:09:06 +0000

    Terraform: SQS Queue Confirmed

--- main.tf
+++ main.tf
@@ -29,3 +29,42 @@ resource "aws_sqs_queue" "receipts_queue" {
     maxReceiveCount     = 3
   })
 }
+
+resource "aws_dynamodb_table" "receipts" {
+  name         = "receipt-processor-results"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "receipt_id"
+
+  attribute {
+    name = "receipt_id"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue_policy" "allow_s3" {
+  queue_url = aws_sqs_queue.receipts_queue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "s3.amazonaws.com" }
+      Action    = "sqs:SendMessage"
+      Resource  = aws_sqs_queue.receipts_queue.arn
+      Condition = {
+        ArnEquals = { "aws:SourceArn" = aws_s3_bucket.receipts.arn }
+      }
+    }]
+  })
+}
+
+resource "aws_s3_bucket_notification" "uploads" {
+  bucket = aws_s3_bucket.receipts.id
+
+  queue {
+    queue_arn = aws_sqs_queue.receipts_queue.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_sqs_queue_policy.allow_s3]
+}
