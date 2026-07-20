5acfd17a4bacc3aa65198d1fc92cf61f50bceb80
neven <nevens2003@gmail.com>
2026-07-20 05:02:21 +0000

    Terraform: S3 upload bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "receipts" {
+  bucket = "receipt-processor-nevenspooner"
+}
