1be799a01cc9e213e5112d92b2730bd1406bd644
neven <nevens2003@gmail.com>
2026-07-22 02:38:56 +0000

    Add S3-hosted frontend

--- main.tf
+++ main.tf
@@ -254,3 +254,59 @@ resource "aws_lambda_permission" "api_invoke" {
 output "api_url" {
   value = "${aws_apigatewayv2_stage.default.invoke_url}/receipts"
 }
+
+# ---------- Frontend ----------
+
+resource "aws_s3_bucket" "frontend" {
+  bucket = "receipt-processor-site-nevenspooner"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket                  = aws_s3_bucket.frontend.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_public" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.frontend]
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.frontend.id
+  key          = "index.html"
+  content_type = "text/html"
+
+  content = templatefile("${path.module}/frontend/index.html.tftpl", {
+    api_url = "${aws_apigatewayv2_stage.default.invoke_url}/receipts"
+  })
+
+  etag = md5(templatefile("${path.module}/frontend/index.html.tftpl", {
+    api_url = "${aws_apigatewayv2_stage.default.invoke_url}/receipts"
+  }))
+}
+
+output "site_url" {
+  value = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
+}
